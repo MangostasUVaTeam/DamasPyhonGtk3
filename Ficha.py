aa58c05906b8f62c381c31ae5426b9393130c1ac
Sergio García <garciparedes@gmail.com>
2014-05-15 09:13:25 +0200

    CSS Adri

--- Ficha.py
+++ Ficha.py
@@ -1,29 +1,50 @@
-﻿class Ficha:
-	""" Clase Ficha de la que se van a crear objetos """
-	""" Cada objeto Ficha representa un peon o dama del tablero colocados en una posicion del mismo."""
+﻿#Adrian Calvo Rojo y Sergio Garcia Prado
+#!/usr/bin/python
+import gtk
+
+class Casilla(gtk.Button):
 	
 	#Inicializar cada objeto
-	def __init__(self, color, posicion, tipo):
-
+	def __init__(self, color, tipo, posicion, vacia):
+		super(Casilla, self).__init__()
 		#Las fichas negras = 0 y las blancas = 1
 		self.color = color
-
-		self.posicion = posicion
 		
 		#Las fichas = 0 y las damas = 1
 		self.tipo = tipo
 
+		self.posicion = posicion
 
-	#Funcion que devuelve las fichas para ser impresas
-	def verFicha(self):
+		self.seleccionado = False
 
-		if self.color == 0:
-			if self.tipo == 0:
-				return "*"
-			else:
-				return "X"
+		self.vacia = vacia
+
+		super(Casilla, self).set_size_request(60,60)
+
+		if vacia:
+			super(Casilla, self).set_name('Vacia')
 		else:
-			if self.tipo == 0:
-				return "o"
-			else:
-				return "8"
+			if color  == 0:
+				if tipo == 0:
+					super(Casilla, self).set_name('FichaNegra')
+				else:
+					super(Casilla, self).set_name('FichaNegraDama')
+			if color  == 1:
+				if tipo == 0:
+					super(Casilla, self).set_name('FichaBlanca')
+				else:
+					super(Casilla, self).set_name('FichaBlancaDama')
+
+	def reset(self):
+		self.seleccionado = False
+		if not self.vacia:
+			if self.color  == 0:
+				if self.tipo == 0:
+					self.set_name('FichaNegra')
+				else:
+					self.set_name('FichaNegraDama')
+			if self.color  == 1:
+				if self.tipo == 0:
+					self.set_name('FichaBlanca')
+				else:
+					self.set_name('FichaBlancaDama')
